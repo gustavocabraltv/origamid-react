00d2933868d2332308079de09e61120a4ebcf540
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-08-30 10:59:54 -0300

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Login from './components/Login'
+
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Origamid</h1>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/> 
+         <Route path='/login' element={<Login/>}/> 
+      </Routes>
+     
+
+      <Footer/>
+    </Router>
+    
   );
 }
 
