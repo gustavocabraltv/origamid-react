6c0e97ab8cf764d5b394709dd9ffb5a866c31baa
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-08-30 15:00:53 -0300

    Logo added in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom' 
 import styles from './Header.module.css'
+import {ReactComponent as Dogs} from '../Assets/dogs.svg'
 
 const Header = () => {
     return(
     <div className={styles.header}>
         <nav className='container'>
-            
+        <Link className={styles.logo} to='/' arial-label='Dogs - Home'>
+            <Dogs/>
+        </Link>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
 
