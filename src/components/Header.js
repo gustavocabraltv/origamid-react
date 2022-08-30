d083dccbed6b5dfb158ec8f45c64feed1b7bea8a
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-08-30 15:47:43 -0300

    Header Styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ import {ReactComponent as Dogs} from '../Assets/dogs.svg'
 const Header = () => {
     return(
     <div className={styles.header}>
-        <nav className='container'>
+        <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to='/' arial-label='Dogs - Home'>
             <Dogs/>
         </Link>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+  
+        <Link className={styles.login} to="/login">Login | Criar</Link>
 
         </nav>
     </div>)
