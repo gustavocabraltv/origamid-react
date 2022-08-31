8f7556d68edcfae9f9b376bb77a9e9014a146e99
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-08-31 19:11:33 -0300

    Button, input, login

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import styles from './Button.module.css'
+import styles from './Input.module.css'
 
-const Input = () => {
+const Input = ({label, type, name}) => {
     return(
-        <div>
-             <input className={styles.input}></input> 
+        <div className={styles.wrapper}>
+             <label htmlFor='' className={styles.label}>{label}</label>
+             <input id={name} name={name} type={type} className={styles.input}/>
+             <p className={styles.error}>Error</p>
         </div>
     )
 }
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Forms/Button';
 import Input from '../Forms/Input' 
 
 const LoginForm = () => {   
@@ -27,7 +28,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input />
+        <Input label='Usuario' type='text' name='username'/>
+        <Input label='Senha' type='password'  name='password'/>
         <input
           type="text"
           value={username}
@@ -38,7 +40,8 @@ const LoginForm = () => {
           value={password}
           onChange={({ target }) => setPassword(target.value)}
         />
-        <button>Entrar</button>
+        <Button>Entrar</Button>
+        
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
